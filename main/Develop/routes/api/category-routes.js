948287e7ea3fe9bc30e71cd287760d9d1c56b229
kferg357 <kennethferguson90@gmail.com>
2021-06-28 15:51:55 -0400

    added to cat-routes

--- main/Develop/routes/api/category-routes.js
+++ main/Develop/routes/api/category-routes.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
   Category.findAll({
-    attributes:['id', 'category_name'],
+    attributes: ['id', 'category_name'],
     include: [
       {
         model: Product,
@@ -15,28 +15,51 @@ router.get('/', (req, res) => {
       }
     ]
   })
-})
-.then(dbcategoryData => res.json(dbcategoryData))
-.catch(err => {
-  console.log(err);
-  res.status(500).json(err);
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'category_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_name']
+      }
+    ]
+  })
+    .then(dbCategoryData => {
+      if (!dbcategoryData) {
+        res.status(404).json({ message: 'No category found with this id' });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.post('/', (req, res) => {
-  // create a new category
-});
+  router.post('/', (req, res) => {
+    // create a new category
+  });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-});
+  router.put('/:id', (req, res) => {
+    // update a category by its `id` value
+  });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
-});
+  router.delete('/:id', (req, res) => {
+    // delete a category by its `id` value
+  });
 
-module.exports = router;
+  module.exports = router;
